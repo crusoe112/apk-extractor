9ecbe68e2d715cd847a57dc7695e5440a31ee6c8
Marc Bohler <mbohler@vigilantsys.com>
2021-08-20 15:28:44 -0500

    Added comments

--- apk-extractor.sh
+++ apk-extractor.sh
@@ -12,33 +12,40 @@ echo ""
 apkUrl=$1
 echo "Google Play URL: $apkUrl"
 
+# Get the download link from Evozi
 downloadLink="`$BASE_PATH/utilities/get_apk_link.py \"$apkUrl\"`"
 echo "APK Download Link: $downloadLink"
 echo ""
 
+# Download the APK
 echo "Downloading APK..."
 wget "$downloadLink"
 
+# Set the APK Filename
 echo ""
 apkFile="`echo ${downloadLink##*/}`"
 echo "APK downloaded to $apkFile"
 echo ""
 
+# Create a JAR file for dex2jar
 jarFile=$apkFile.jar
 echo "Created JAR File: $jarFile"
 echo ""
 
+# Disassemble with akptook
 echo .........apktool..........
 cd $(dirname $apkFile)
 apktool d -d -f "$apkFile"
 echo ..........................
 echo ""
 
+# Convert DEX files to JAR file
 echo .........dex2jar..........
 d2j-dex2jar -f "$apkFile" -o "$jarFile"
 echo ..........................
 echo ""
 
+# Open with JD-GUI
 echo .........jd-gui...........
 java -jar "$BASE_PATH/utilities/jd-gui-1.6.6.jar" "$jarFile"
 echo ..........................
--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
+# Make executable
 chmod +x apk-extractor.sh
 chmod +x ./utilities/get_apk_link.py
+
+# Install apt tools
 sudo apt install -y apktool
 sudo apt install -y dex2jar
+
+# Install JD-GUI
 wget -O ./utilities/jd-gui-1.6.6.jar https://github.com/java-decompiler/jd-gui/releases/download/v1.6.6/jd-gui-1.6.6.jar
